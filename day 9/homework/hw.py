da2da6a3337f259d05c0a297865600508fa969c2
luka-matsaberidze <l.ukamatsaberidze333@gmail.com>
2024-07-21 21:16:11 +0300

    second commit\

--- day 9/homework/hw.py
+++ day 9/homework/hw.py
@@ -1,1 +1,13 @@
-print ("hello world")
+
+
+
+
+
+
+
+a = int(input("enter you age: "))
+b = int(input("how many years do you wanna back?: "))
+c = int(input("which years it is?: "))
+d =int(input("which years do you wanna back?: "))
+print(a-b)
+print(c-b)
