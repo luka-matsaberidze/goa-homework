f9394431a55596f7b1e48ef13db4e879d51adab8
luka-matsaberidze <l.ukamatsaberidze333@gmail.com>
2024-07-23 18:19:13 +0300

    second commit

---
+++ day 9/bonus/bonus.py
@@ -0,0 +1,69 @@
+#bonus.1
+
+number = int(input("enter any number: "))
+
+print(number)
+
+
+if number % 2==0:
+    print("its even number")
+else:
+    print("its odd number")
+
+
+#bonus 2
+
+
+number2 = int(input("enter any number again: "))
+
+
+if number2>=0:
+    print("0+")
+elif number2==0:
+    print("its 0")
+elif number2 <0:
+    print("-0")
+
+
+#bonus 3
+
+age = int(input("enter your age: "))
+
+if age>=18:
+    print("you are adult")
+else:
+    print("you are virgin")
+
+#bonus 4
+
+#1 orsabati
+#2 samsabati
+#3 otxsabati
+#4 xutsabati
+#5 paraskevi
+#6 sabati
+#7 kvira
+
+a = int(input("enter any witch week do you want: "))
+
+if a ==1:
+    print("orsabati")
+elif a ==2:
+    print("samsabati")
+elif a ==3:
+    print("otxsabati")
+elif a ==4:
+    print("xutsabati")
+elif a ==5:
+    print("paraskevi")
+elif a ==6:
+    print("sabati")
+elif a ==7:
+    print("kvira")
+
+
+
+
+
+
+
--- day 9/homework/hw.py
+++ day 9/homework/hw.py
@@ -1,13 +1,7 @@
 
-
-
-
-
-
-
 a = int(input("enter you age: "))
 b = int(input("how many years do you wanna back?: "))
 c = int(input("which years it is?: "))
-d =int(input("which years do you wanna back?: "))
+d =int(input("how many years do you wanna back?: "))
 print(a-b)
-print(c-b)
+print(c-d)
