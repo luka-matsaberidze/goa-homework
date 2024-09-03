c4f02347417395b47e39aa55b00f41daffccdac6
luka-macaberidze <luka.matsaberidze333@gmail.com>
2024-09-03 05:58:34 +0400

    second commit

---
+++ day 20/classwork/cw.py
@@ -0,0 +1,26 @@
+#cw.1
+for i in range(0, 100, 5):
+    print(i)
+
+
+#cw.2
+for i in range(1, 100, 2):
+    print(str(i)+". es aris kenti")
+
+for i in range(0, 100, 2):
+    print(str(i)+"luwia")
+
+
+# cw.3
+for i in range(1, 100):
+    if i % 5:
+        print("xutis jeradia")
+    else:
+        print("xutis jeradi araa")
+
+
+
+num=0
+for i in range(1, 100, 5):
+   num = num+i
+   print(num)
