cd1f91268eb4aca22846134b9e1aad7ae87fd170
luka-macaberidze <luka.matsaberidze333@gmail.com>
2024-09-11 06:39:48 +0400

    Second commit

---
+++ day 21/homework/hw.py
@@ -0,0 +1,30 @@
+#hw.1
+name=int(input("enter any number: "))
+
+for i in range(name, 100, name):
+    print(i)
+#hw.2
+name=int(input("enter any number: "))
+for i in range(0, name,):
+    print(i)
+#hw.3
+name=int(input("enter any number: "))
+name1=int(input("enter any number: "))
+print(name + name1  )
+print(name - name1  )
+print(name // name1  )
+print(name / name1  )
+print(name % name1  )
+#hw.4
+name=int(input("enter any number: "))
+for i in range(0, 100,):
+    if name % 2==0:
+        print(str(i)+". luwiaa")
+for i in range(1, 100):
+    if name % 2==1:
+        print(str(i)+". kentia")
+#hw.5
+name=int(input("enter any number: "))
+for i in range(name, 100, 5):
+    print(i)
+
