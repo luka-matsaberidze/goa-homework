bd933159002456dbcf3939c5e46d8014cf605b19
luka-macaberidze <luka.matsaberidze333@gmail.com>
2024-09-25 23:02:58 +0400

    Second commit

---
+++ day 28/classwork/cw.py
@@ -0,0 +1,21 @@
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(num[1])
+# print(num[3])
+# print(num[5])
+# print(num[7])
+# print(num[9])
+
+
+
+# num = ["fasanauri xinkali", "mwvadi", "porse 911 gt3 rs", "bmw m4", "no mersedes", "gtr", "black ninja", "jonny macaberidze", "kitara", "memu", "wiwo" ]
+
+# for i in range(len(num)):
+#     print(num[i])
+
+num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for i in range(len(num)):
+    if num[i] % 2 ==0:
+       print(num[i])
+   
+
